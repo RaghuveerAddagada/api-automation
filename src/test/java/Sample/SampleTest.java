ee41c17863b545ade6a86893f82702df7ed44934
Addagada Raghuveer <addagada.raghuveer@cred.club>
2023-02-28 16:23:29 +0530

    Adding some nit code

--- src/test/java/Sample/SampleTest.java
+++ src/test/java/Sample/SampleTest.java
@@ -5,6 +5,7 @@ import com.backend.apis.GenderAPIs;
 import com.backend.helpers.common.StatusCode;
 import com.backend.pojo.DataUSAResponse;
 import com.backend.pojo.GenderApiResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
@@ -36,5 +37,21 @@ public class SampleTest extends GenderAPIs {
         Reporter.log("Subtopic in Source  Response : " + response.getSource().get(0).getAnnotations().getSubtopic(),true);
         Reporter.log("Topic in Source  Response : " + response.getSource().get(0).getAnnotations().getTopic(),true);
 
+    }
+
+    @Test
+    public void abcdf() {
+        final String getMaskedCardNumber = "XXXX-XXXX-XXXX-1223";
+        final String[] cardNumberAfterSplit = getMaskedCardNumber.split("-");
+
+        Assert.assertEquals(StringUtils.countMatches(cardNumberAfterSplit[0], "XXXX"), 1, "First part of card number is un-masked");
+        Assert.assertEquals(StringUtils.countMatches(cardNumberAfterSplit[1], "XXXX"), 1, "Second part of card number is un-masked");
+        Assert.assertEquals(StringUtils.countMatches(cardNumberAfterSplit[2], "XXXX"), 1, "Third part of card number is un-masked");
+
+        if (cardNumberAfterSplit[3].startsWith("XX")) {
+            Assert.assertTrue(StringUtils.isNumeric(cardNumberAfterSplit[3].replace("XX","")), "Fourth part of card number is masked");
+        } else {
+            Assert.assertTrue(StringUtils.isNumeric(cardNumberAfterSplit[3]), "Fourth part of card number is masked");
+        }
     }
 }
