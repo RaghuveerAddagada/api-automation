e9dabfbbe7cca111816b952752a0abfedb4dce56
Addagada Raghuveer <addagada.raghuveer@cred.club>
2023-03-26 14:54:19 +0530

    Adding checkstyle in place

--- src/main/java/com/backend/apis/GenderAPIs.java
+++ src/main/java/com/backend/apis/GenderAPIs.java
@@ -1,15 +1,17 @@
 package com.backend.apis;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.backend.helpers.common.StatusCode;
 import com.backend.helpers.endpoints.Service;
 import com.backend.helpers.restassured.RestClient;
 import com.backend.pojo.GenderApiResponse;
 import com.jayway.restassured.builder.RequestSpecBuilder;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 
-import static com.backend.helpers.restassured.RequestType.*;
+
+import static com.backend.helpers.restassured.RequestType.GET;
 
 public class GenderAPIs {
 
@@ -20,11 +22,9 @@ public class GenderAPIs {
     public GenderApiResponse getGenderByName(final String name, final StatusCode statusCode) {
 
         log.info(String.format("sending request with name as : %s", name));
-        final RequestSpecBuilder request = new RequestSpecBuilder()
-                .setBaseUri(Service.GENDER.getService())
-                .addQueryParam("name", name);
-                // if you have headers
-                //.addHeaders(Util.getHeaders());
+        final RequestSpecBuilder request = new RequestSpecBuilder().setBaseUri(Service.GENDER.getService()).addQueryParam("name", name);
+        // if you have headers
+        //.addHeaders(Util.getHeaders());
 
         return restClient.getResponse(GET, request.build(), GenderApiResponse.class, statusCode.getCode());
     }
